1e1818b92d8c12ddd61681c371c3d832a5ddb7f7
oleggorkov <ole.gorkov@yandex.ru>
2019-08-06 12:00:49 +0300

    lesson2

---
+++ models/Day.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evg
+ * Date: 03/08/2019
+ * Time: 13:38
+ */
+
+namespace app\models;
+
+use yii\base\Model;
+
+
+class Day extends Model
+{
+    public $id;
+    public $work_day;
+    public $output_day;
+    public $activity;
+
+    }
