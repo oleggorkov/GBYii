c29846949170df74396b8ef91af9b6f36ea365f9
oleggorkov <ole.gorkov@yandex.ru>
2019-08-14 10:11:27 +0300

    lesson3

--- views/activity/view.php
+++ views/activity/view.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: evg
- * Date: 03/08/2019
- * Time: 13:46
- */
 
-/** @var \app\models\Activity $model */
+/* @var $this yii/web/View */
 
-var_dump($model->id);
-var_dump($model['id']);
-foreach ($model as $attribute => $value){
-    echo $model->getAttributeLabel($attribute).":".$value. "<br/>";
+var_dump($model);
+?>
+
+<h1>activity/view</h1>
+
 }
